913f60688202e2771abb694fc222c5a44f261447
arunsunhare5678@gmail.com <arunsunhare@Aruns-MacBook-Air.local>
2024-06-08 00:32:25 +0530

    Add text area in this and upercase add

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
    <Navbar /> */}
    <Navbar title='TextUtils' abouttext='About TextUtils'/>
    <div className="container my-3">
-    <TextForm heading='Enter the text to analyze'/>
+    <TextForm heading='Enter the text to analyze below'/>
    </div>
    
    
--- src/componets/TextForm.js
+++ src/componets/TextForm.js
@@ -2,15 +2,25 @@
  
  
  function TextForm(props) {
-    const [text,setText]= useState('Enter text here');
-
+    const [text,setText]= useState('Enter text here2');
+    const handleUpClick =()=>{
+      // console.log("Uppercase was clicked" + text);
+      let newText = text.toUpperCase();
+      setText(newText)
+    }
+    const handleonChange =(event)=>{
+      // console.log("OnChange");
+      setText(event.target.value);
+    }
    return (
      <div>
    <h1>{props.heading}</h1>
 <div class="mb-3">
 
-  <textarea className="form-control" id="myBox" rows="15"></textarea>
-  <button className='btn btn-primary my-3'>Convert to Upercase</button>
+  <textarea className="form-control" value={text} onChange={handleonChange} id="myBox" rows="15">
+
+  </textarea>
+  <button className='btn btn-primary my-3'onClick={handleUpClick}>Convert to Upercase</button>
 </div>
      </div>
    )
