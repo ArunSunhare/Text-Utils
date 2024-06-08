28163d3a6eafc180d16b039af24ce01d7fe35f49
arunsunhare5678@gmail.com <arunsunhare@Aruns-MacBook-Air.local>
2024-06-08 21:40:02 +0530

    Add lowercase button and clear button

--- src/componets/TextForm.js
+++ src/componets/TextForm.js
@@ -2,10 +2,20 @@
  
  
  function TextForm(props) {
-    const [text,setText]= useState('Enter text here2');
+    const [text,setText]= useState('');
     const handleUpClick =()=>{
       // console.log("Uppercase was clicked" + text);
       let newText = text.toUpperCase();
+      setText(newText)
+    }
+    const handleLoClick =()=>{
+      // console.log("Uppercase was clicked" + text);
+      let newText = text.toLowerCase();
+      setText(newText)
+    }
+    const handleClearClick =()=>{
+      // console.log("Uppercase was clicked" + text);
+      let newText =" ";
       setText(newText)
     }
     const handleonChange =(event)=>{
@@ -13,16 +23,30 @@
       setText(event.target.value);
     }
    return (
-     <div>
+    <>
+     <div className='container'>
    <h1>{props.heading}</h1>
 <div class="mb-3">
 
-  <textarea className="form-control" value={text} onChange={handleonChange} id="myBox" rows="15">
+  <textarea className="form-control my-2" value={text} onChange={handleonChange} id="myBox" rows="15">
 
   </textarea>
-  <button className='btn btn-primary my-3'onClick={handleUpClick}>Convert to Upercase</button>
+  <button className='btn btn-primary mx-2'onClick={handleUpClick}>Convert to Upercase</button>
+  <button className='btn btn-primary mx-2'onClick={handleLoClick}>Convert to Lowercase</button>
+  <button className='btn btn-primary mx-2'onClick={handleClearClick}>Clear Text</button>
+  
 </div>
      </div>
+    
+    <div className='contianer my-2'> 
+     <h3>Your text summary</h3>
+     <p>{ text.split (" ").length} words and {text.length}characters</p>
+     <p>{0.008 * text.split (" ").length} Minutes read</p>
+     <h2>Preview</h2>
+     <p>{text}</p>
+    </div>
+</>
+     
    )
  }
  
