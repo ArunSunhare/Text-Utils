afa473967519690f1ec06968b4336a66cc2b77c3
arunsunhare5678@gmail.com <arunsunhare@Aruns-MacBook-Air.local>
2024-06-09 23:15:55 +0530

    add about

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+// import About from './componets/About';
 import Navbar from './componets/Navbar';
 import TextForm from './componets/TextForm'
 function App() {
@@ -9,6 +10,8 @@ function App() {
    <Navbar title='TextUtils' abouttext='About TextUtils'/>
    <div className="container my-3">
     <TextForm heading='Enter the text to analyze below'/>
+    {/* <About/> */}
+
    </div>
    
    
